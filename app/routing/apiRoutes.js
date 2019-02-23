5b6cdeb0af478a2e6f7fb974b4c1836f48943568
DoleTM <adylantoremember@gmail.com>
2019-02-23 12:26:17 -0500

    routes coded and html started

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,10 @@
+var friends = require("../data/friends");
+var path = require("path");
+
+app.get("/api/friends", function(req,res){
+    res.json(friends);
+});
+
+app.post('/api/friends', function(req,res){
+    friends.push(req.body);
+});
